fc82e7e16bb3aaea87e2b8c30f765fa80bbdc056
Gavin Nitta <gshigeto@gmail.com>
2017-09-21 22:18:48 -0600

    Add Animate CSS

--- config/copy.config.js
+++ config/copy.config.js
@@ -2,5 +2,10 @@ var path = require('path');
 var defaultConfig = require('@ionic/app-scripts/config/copy.config.js');
 
 module.exports = function () {
+  defaultConfig.AnimateCSS = {
+    src: ['{{ROOT}}/node_modules/animate.css/animate.min.css'],
+    dest: '{{BUILD}}'
+  };
+
   return defaultConfig;
 };
